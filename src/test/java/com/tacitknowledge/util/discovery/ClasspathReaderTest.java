1e8a7512d05f7d79b2b46d5ad010eaf509c78be0
vpertu <vpertu@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-10-16 08:38:21 +0000

    Increased the timeout before reloading the class, on a faster machine (host vs VM) same timeout fails the test, since time seem to coincide.
    git-svn-id: https://svn.tacitknowledge.com/svn/common/trunk/discovery@1478 1caace6a-b83d-0410-a556-d4fd5157a890

--- src/test/java/com/tacitknowledge/util/discovery/ClasspathReaderTest.java
+++ src/test/java/com/tacitknowledge/util/discovery/ClasspathReaderTest.java
@@ -1,9 +1,9 @@
 /* Copyright 2007 Tacit Knowledge LLC
- * 
+ *
  * Licensed under the Tacit Knowledge Open License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License. You may
  * obtain a copy of the License at http://www.tacitknowledge.com/licenses-1.0.
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,22 +13,22 @@
 
 package com.tacitknowledge.util.discovery;
 
+import junit.framework.TestCase;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-import junit.framework.TestCase;
-
 /**
  * Test the ClasspathReader
- * 
+ *
  * @author Mike Hardy <mailto:mike@tacitknowledge.com/>
  */
 public class ClasspathReaderTest extends TestCase
 {
     /** The reader we're testing */
     private ClasspathFileReader reader = new ClasspathFileReader();
-    
+
     /**
      * Test loading a non-existent class
      */
@@ -48,10 +48,10 @@ public class ClasspathReaderTest extends TestCase
             fail("Should have been a FileNotFoundException, was: " + e.getClass().getName());
         }
     }
-    
+
     /**
      * Test loading a file from the classpath
-     * 
+     *
      * @exception Exception if anything goes wrong
      */
     public void testBasicFileLoad() throws Exception
@@ -62,13 +62,13 @@ public class ClasspathReaderTest extends TestCase
                        + "util" + File.separator
                        + "discovery" + File.separator
                        + "ClasspathFileReader.class");
-        
+
         // TODO actually assert something - harder than it sounds
     }
-    
+
     /**
      * Test file loading as a stream
-     * 
+     *
      * @exception Exception if anything goes wrong
      */
     public void testBasicStreamLoad() throws Exception
@@ -80,13 +80,13 @@ public class ClasspathReaderTest extends TestCase
                                                         + "discovery" + File.separator
                                                         + "ClasspathFileReader.class");
         stream.close();
-        
+
         // TODO actually assert something - harder than it sounds
     }
-    
+
     /**
      * Test modification access
-     * 
+     *
      * @exception Exception if anything goes wrong
      */
     public void testModification() throws Exception
@@ -97,7 +97,7 @@ public class ClasspathReaderTest extends TestCase
                                    + "util" + File.separator
                                    + "discovery" + File.separator
                                    + "ClasspathFileReader.class");
-        
+
         // no reload time, but no modification, should be false
         reader.setReloadSeconds(0);
         boolean modified = reader.isModified("com" + File.separator
@@ -106,7 +106,7 @@ public class ClasspathReaderTest extends TestCase
                                              + "discovery" + File.separator
                                              + "ClasspathFileReader.class");
         assertFalse(modified);
-        
+
         // alter the file, set a reload time, should be false
         file.setLastModified(System.currentTimeMillis());
         reader.setReloadSeconds(1);
@@ -117,9 +117,9 @@ public class ClasspathReaderTest extends TestCase
                                      + "discovery" + File.separator
                                      + "ClasspathFileReader.class");
         assertFalse(modified);
-        
+
         // wait for reload time to expire, should be true
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         modified = reader.isModified("com" + File.separator
                                      + "tacitknowledge" + File.separator
                                      + "util" + File.separator
